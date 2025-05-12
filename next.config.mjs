296f9180e7d977c7781da8dccf74e5cc621b981b
A Sendou <thesendou@gmail.com>
2025-05-12 10:22:25 +0700

    deploy script

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // ⬅️ penting agar bisa static export
+  trailingSlash: true, // ⬅️ biar semua URL jadi path folder (SEO + Netlify friendly)
+
   eslint: {
     ignoreDuringBuilds: true,
   },
